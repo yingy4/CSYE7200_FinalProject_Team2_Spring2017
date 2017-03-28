84d0cd9787e5ce01e1c06841eed4f482acf5f1f9
Wei Huang <huang.wei3@husky.neu.edu>
2017-03-28 18:33:33 -0400

    add Spark dependencies in build.sbt. Add a simple Spark test run

--- build.sbt
+++ build.sbt
@@ -16,8 +16,9 @@ libraryDependencies ++= Seq(
   "oauth.signpost" % "signpost-commonshttp4" % "1.2",
   "org.apache.httpcomponents" % "httpclient" % "4.5",
   "org.apache.httpcomponents" % "httpcore" % "4.4.6",
-  "org.apache.commons" % "commons-io" % "1.3.2")
+  "org.apache.commons" % "commons-io" % "1.3.2",
+  "org.apache.spark" %% "spark-core" % "2.1.0")
 
 val sprayGroup = "io.spray"
 val sprayJsonVersion = "1.3.2"
-libraryDependencies ++= List("spray-json") map {c => sprayGroup %% c % sprayJsonVersion}
+libraryDependencies ++= List("spray-json") map { c => sprayGroup %% c % sprayJsonVersion }
--- src/main/scala/edu/neu/coe/scala/retrieval/TwitterClient.scala
+++ src/main/scala/edu/neu/coe/scala/retrieval/TwitterClient.scala
@@ -1,4 +1,5 @@
 package edu.neu.coe.scala.retrieval
+
 import oauth.signpost.commonshttp.CommonsHttpOAuthConsumer
 import org.apache.commons.io.IOUtils
 import org.apache.http.client.methods.HttpGet
@@ -6,6 +7,10 @@ import org.apache.http.impl.client.{DefaultHttpClient, HttpClientBuilder}
 import twitter4j.TwitterFactory
 import twitter4j.conf.ConfigurationBuilder
 import twitter4j.Twitter
+
+import org.apache.spark._
+import org.apache.spark.SparkContext._
+import org.apache.log4j._
 
 /**
   * Created by Mushtaq on 3/26/2017.
@@ -19,17 +24,46 @@ object TwitterClient {
   val AccessSecret = "7XfA0v9j0utKeUuf44n2YEB3AtzqVlMM0ue4IrJC0v2cK"
 
   def main(args: Array[String]) {
-  
+
     val consumer = new CommonsHttpOAuthConsumer(ConsumerKey, ConsumerSecret)
-  consumer.setTokenWithSecret(AccessToken, AccessSecret)
-  val request = new HttpGet("https://api.twitter.com/1.1/search/tweets.json?q=Northeastern%20University")
-  consumer.sign(request)
-  val client = HttpClientBuilder.create().build()
-  val response = client.execute(request)
+    consumer.setTokenWithSecret(AccessToken, AccessSecret)
+    val request = new HttpGet("https://api.twitter.com/1.1/search/tweets.json?q=Northeastern%20University")
+    consumer.sign(request)
+    val client = HttpClientBuilder.create().build()
+    val response = client.execute(request)
 
-  println(response.getStatusLine().getStatusCode())
+    println(response.getStatusLine().getStatusCode())
 
-  println(IOUtils.toString(response.getEntity().getContent()))
-}
+    println(IOUtils.toString(response.getEntity().getContent()))
+
+    /**
+      * Spark test run, counts the number of ratings (1 - 5) in testdata/u.data
+      * by Wei Huang, Mar. 28
+      */
+
+    println("\nSpark test run\n")
+
+    // Set the log level to only print errors
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    // Create a SparkContext using every core of the local machine, named RatingsCounter
+    val sc = new SparkContext("local[*]", "RatingsCounter")
+
+    // Load up each line of the ratings data into an RDD
+    val lines = sc.textFile("testdata/u.data")
+
+    // Convert each line to a string, split it out by tabs, and extract the third field.
+    // (The file format is userID, movieID, rating, timestamp)
+    val ratings = lines.map(x => x.toString().split("\t")(2))
+
+    // Count up how many times each value (rating) occurs
+    val results = ratings.countByValue()
+
+    // Sort the resulting map of (rating, count) tuples
+    val sortedResults = results.toSeq.sortBy(_._1)
+
+    // Print each result on its own line.
+    sortedResults.foreach(println)
+  }
 
 }
